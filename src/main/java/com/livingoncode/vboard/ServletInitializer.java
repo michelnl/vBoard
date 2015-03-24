e99f444dcff9fc57c2dc9df77b8abd4865ebca83
unknown <michel.grin@MW7A67CZ3FIVMQ.dir.svc.accenture.com>
2015-03-24 15:04:38 +0100

    first commit

---
+++ src/main/java/com/livingoncode/vboard/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.livingoncode.vboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VBoardApplication.class);
+	}
+
+}
